7eba00bae84a8a3100a7331e04d41928a9ad8929
nttai0207-maker <nttai02.07@gmail.com>
2026-09-18 20:23:09 +0700

    Java

---
+++ JavaModule1/src/phan01/Bai01.java
@@ -0,0 +1,8 @@
+package phan01;
+
+public class Bai01 {	
+	  //Xuất ra chữ Hello World!		
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ JavaModule1/src/phan02/ConditionalDemo.java
@@ -0,0 +1,16 @@
+package phan02;
+
+public class ConditionalDemo {
+	public static void main(String[] args) {
+        int value1 = 1; 
+        int value2 = 2; 
+        int result;
+        
+        boolean someCondition = true;
+               
+        result = someCondition ? value1 : value2;
+        
+        System.out.println(result);
+    }
+
+}
